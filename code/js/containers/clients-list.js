4995a1034fcdacd8f0dc9a151cb1d6d598397a8d
dabdima <baristin43@gmail.com>
2018-05-19 16:12:32 +0300

    working on client li

--- code/js/containers/clients-list.js
+++ code/js/containers/clients-list.js
@@ -8,7 +8,15 @@ class ClientsList extends Component {
     showList () {
         return this.props.clients.map ((client) => {
             return (
-                <li onClick={() => this.props.select (client)} key={client.id}>{client.general.firstName}</li>
+                <li className="client-block" onClick={() => this.props.select (client)} key={client.id}>
+                    <div className="client-block-avatar">
+                        <img src={client.general.avatar} width="128px" height="128px" alt=""/>
+                    </div>
+                    <div className="client-block-inf">
+                        <h3>{client.general.firstName} {client.general.lastName}</h3>
+                        <p>{client.job.title}</p>
+                    </div>
+                </li>
             );
         });
     }
